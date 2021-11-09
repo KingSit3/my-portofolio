9273c00ca19d07ce337cae6d4144715ad5a227c4
KingSit3 <i.ardiansyah93@gmail.com>
2021-11-09 11:34:55 +0700

    Lets migration

--- src/views/Front.vue
+++ src/views/Front.vue
@@ -6,13 +6,13 @@
       <div class="mb-10 text-center space-y-3">
         <span class="text-3xl font-bold capitalize">Anime@MyPonyAsia</span>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-9 font-bold justify-items-center">
-            <router-link to="/myponyasia" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Switcher</router-link>
+            <!-- <router-link to="/myponyasia" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Switcher</router-link>
             <router-link to="/myponyasia/home" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Main Page </router-link>
             <router-link to="/myponyasia/anime2" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Anime Page V2</router-link>
             <router-link to="/myponyasia/anime" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Anime Page </router-link>
             <router-link to="/myponyasia/streaming" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Streaming Page </router-link>
             <router-link to="/myponyasia/streaming2" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Streaming Page V2 </router-link>
-            <router-link to="/myponyasia/Find" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Find Page </router-link>
+            <router-link to="/myponyasia/Find" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Find Page </router-link> -->
         </div> 
       </div>
       <!-- End Anime MyPonyAsia -->
@@ -21,8 +21,8 @@
       <div class="mb-10 text-center space-y-3">
         <span class="text-3xl font-bold capitalize">Pony@MyPonyAsia</span>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-9 font-bold justify-items-center">
-            <router-link to="/myponyasia" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Switcher</router-link>
-            <router-link to="/myponyasia/pony/home" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Main Page  </router-link>
+            <!-- <router-link to="/myponyasia" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Switcher</router-link>
+            <router-link to="/myponyasia/pony/home" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Main Page  </router-link> -->
         </div> 
       </div>
       <!-- End Pony MyPonyAsia -->
@@ -31,10 +31,10 @@
       <div class="my-10 text-center space-y-3">
         <span class="text-3xl font-bold capitalize">Admin Template</span>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-9 font-bold justify-items-center">
-            <router-link to="/admin1" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Admin 1 </router-link>
+            <!-- <router-link to="/admin1" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Admin 1 </router-link>
             <router-link to="/admin2" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Admin 2 </router-link>
             <router-link to="/admin3" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Admin 3 </router-link>
-            <router-link to="/admin4" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Admin 4 </router-link>
+            <router-link to="/admin4" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center"> Admin 4 </router-link> -->
         </div> 
       </div>
       <!-- End Admin Template -->
@@ -43,8 +43,8 @@
       <div class="my-10 text-center space-y-3">
         <span class="text-3xl font-bold capitalize">Landing Page</span>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-9 font-bold justify-items-center">
-            <router-link to="/landingpage1" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Landing Page 1</router-link>
-            <router-link to="/landingpage2" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Landing Page 2</router-link>
+            <!-- <router-link to="/landingpage1" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Landing Page 1</router-link>
+            <router-link to="/landingpage2" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Landing Page 2</router-link> -->
         </div> 
       </div>
       <!-- End Landing Page -->
@@ -53,9 +53,9 @@
       <div class="my-10 text-center space-y-3">
         <span class="text-3xl font-bold">Gabut :v</span>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-9 font-bold justify-items-center">
-            <router-link to="/spotify2021" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Spotifiru 2021</router-link>
+            <!-- <router-link to="/spotify2021" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Spotifiru 2021</router-link>
             <router-link to="/netfliksu2021" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Netfliksu 2021</router-link>
-            <router-link to="/restoran" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Restoran</router-link>
+            <router-link to="/restoran" class="py-10 w-3/4 bg-purple-700 rounded-lg text-center">Restoran</router-link> -->
         </div> 
       </div>
       <!-- End Gabut Project -->
@@ -72,9 +72,9 @@
 
       <!-- Project -->
       <div class="my-10 text-center space-y-3">
-        <span class="text-3xl font-bold">Zakat</span>
+        <span class="text-3xl font-bold">Project</span>
         <div class="grid grid-cols-2 md:grid-cols-4 gap-9 font-bold justify-items-center">
-            <router-link to="/digitascientia" class="shadow-red py-10 w-3/4 bg-purple-700 rounded-lg text-center">digitascientia</router-link>
+            <router-link to="/digitascientia" class="shadow-red py-10 w-3/4 bg-purple-700 rounded-lg text-center">Digitascientia</router-link>
         </div> 
       </div>
       <!-- End Project -->
