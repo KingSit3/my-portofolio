0b2277ee45c37c42d68da2bae91e06cb27af4cd3
KingSit3 <i.ardiansyah93@gmail.com>
2021-11-12 06:34:32 +0700

    add Pagination Component

---
+++ src/components/myponyasia/pony/parts/Pagination1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="max-w-[90%] mx-auto my-5">
+    <ul class="flex justify-center items-center space-x-3 font-semibold">
+      <li>
+        <a href="#" class="p-2 px-3 hover:text-[#881798] duration-200">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+          </svg>
+        </a>
+      </li>
+
+      <!-- active -->
+      <li><a href="#" class="p-2 px-4 rounded-full bg-[#881798]">1</a></li>
+      <!-- End active -->
+
+      <li><a href="#" class="p-2 px-3 hover:border-[#881798] border-b-2 border-transparent duration-200">2</a></li>
+      <li class="cursor-default">...</li>
+      <li><a href="#" class="p-2 px-3 hover:border-[#881798] border-b-2 border-transparent duration-200">4</a></li>
+      <li><a href="#" class="p-2 px-3 hover:border-[#881798] border-b-2 border-transparent duration-200">5</a></li>
+      <li>
+        <a href="#" class="p-2 px-3 hover:text-[#881798] duration-200">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
